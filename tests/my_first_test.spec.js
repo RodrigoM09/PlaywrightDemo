8ff63bf9047627d849a3b166382632dabf6270ad
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-04-17 20:52:17 -0500

    auto save

---
+++ tests/my_first_test.spec.js
@@ -0,0 +1,20 @@
+// This is a Playwright Test file. Learn more at https://playwright.dev/docs/test-intro
+
+const { test, expect } = require("@playwright/test");
+
+
+test('My first test', () => {
+    
+});
+
+
+
+
+
+
+
+
+// const { hello, helloWorld } = require("./demo/hello.js");
+
+// console.log(hello());
+// console.log(helloWorld());
